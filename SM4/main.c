d97218499b433d7e858996cb6546bb8c0e946e93
ohmygodlin <laitaizong@gmail.com>
2018-08-03 12:57:32 +0800

    support cbc initial value and refector use crypt

--- SM4/main.c
+++ SM4/main.c
@@ -8,10 +8,24 @@
 
 void usage()
 {
-	printf("Usage: sm4 [-e(encrypt)|-d(decrypt)]\n");
+	printf("Usage: sm4 [-e(encrypt)|-d(decrypt)|-c(cbc_encrypt)|-b(cdc_decrypt]\n");
 }
 
-void encrypt()
+unsigned char iv[16];
+void read_iv()
+{
+  char str[20];
+  int i;
+  printf("\ninput 128-bit initial value:");
+	scanf("%s", str);
+  for(i = 0; i < 16; i++)
+    sscanf(str+i*2, "%02x", &iv[i]);
+}
+
+#define PLAIN_STR "plain"
+#define CIPHER_STR "cipher"
+
+void crypt(int mode)
 {
 	unsigned char key[16];
   unsigned char input[MAX_SIZE];
@@ -23,7 +37,7 @@ void encrypt()
   memset(input, 0, MAX_SIZE);
   memset(output, 0, MAX_SIZE);
   
-  printf("\ninput plaintext(in hex):");
+  printf("\ninput %stext(in hex):", (mode == SM4_ENCRYPT) ? PLAIN_STR : CIPHER_STR);
   scanf("%s", str);
 	while(sscanf(str+len*2, "%02x", &(input[len]))!=EOF)
 		len++;
@@ -34,45 +48,22 @@ void encrypt()
 		sscanf(str+i*2, "%02x", &key[i]);
     
   //encrypt standard testing vector
-	sm4_setkey_enc(&ctx,key);
-	sm4_crypt_ecb(&ctx,SM4_ENCRYPT,len,input,output);
-  printf("\nciphertext = ");
+  if (mode == SM4_ENCRYPT)
+    sm4_setkey_enc(&ctx,key);
+  else
+    sm4_setkey_dec(&ctx,key);
+  
+  if (strlen(iv))
+    sm4_crypt_cbc(&ctx,mode,len,iv,input,output);
+  else
+    sm4_crypt_ecb(&ctx,mode,len,input,output);
+  
+  printf("\n%stext = ", (mode == SM4_ENCRYPT) ? CIPHER_STR : PLAIN_STR);
 	for(i=0;i<len;i++)
 		printf("%02x", output[i]);
 	printf("\n");
-
-}
-
-void decrypt()
-{
-	unsigned char key[16];
-  unsigned char input[MAX_SIZE];
-  unsigned char output[MAX_SIZE];
-  char str[MAX_SIZE * 2];
-  sm4_context ctx;
-  int len = 0, i;
-  
-  memset(input, 0, MAX_SIZE);
-  memset(output, 0, MAX_SIZE);
-  
-  printf("\ninput ciphertext(in hex):");
-  scanf("%s", str);
-	while(sscanf(str+len*2, "%02x", &(input[len]))!=EOF)
-		len++;
-  
-  printf("\ninput 128-bit secret key(in hex):");
-	scanf("%s", str);
-  for(i = 0; i < 16; i++)
-		sscanf(str+i*2, "%02x", &key[i]);
-    
-  //encrypt standard testing vector
-	sm4_setkey_dec(&ctx,key);
-	sm4_crypt_ecb(&ctx,SM4_DECRYPT,len,input,output);
-  printf("\nplainnum = ");
-	for(i=0;i<len;i++)
-		printf("%02x", output[i]);
-	printf("\nplaintext = %s\n", output);
-
+  if (mode == SM4_DECRYPT)
+    printf("\nplaintext = %s\n", output);
 }
 
 int main(int argc, char **argv)
@@ -87,15 +78,22 @@ int main(int argc, char **argv)
 		usage();
 		return 1;
 	}
+  memset(iv, 0, sizeof(iv));
 	switch(argv[1][1])
 	{
+    case 'c':
+    case 'C':
+      read_iv();
 		case 'e':
 		case 'E': 
-			encrypt();
+			crypt(SM4_ENCRYPT);
 			break;
-		case 'd':
+		case 'b':
+		case 'B':
+      read_iv();
+    case 'd':
 		case 'D':
-			decrypt();
+			crypt(SM4_DECRYPT);
 			break;
 		default:
 			usage();
